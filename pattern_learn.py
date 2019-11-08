d542fb1c61b0b2b540c4e6a1a8d9c1df8f8b8b80
ehauckdo <eduardohauck@gmail.com>
2019-11-08 10:47:23 +0900

    Add point selection function

--- pattern_learn.py
+++ pattern_learn.py
@@ -5,6 +5,8 @@ import optparse
 import math
 import logging, inspect
 
+from point_selection import get_points
+
 # set up logger
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -58,21 +60,18 @@ def read_map(path):
 
 	return map_struct
 
-def get_points(map_data):
-	n_cols = map_data.n_cols
-	n_rows = map_data.n_rows
-	print(n_cols, n_rows)
-
 if __name__ == '__main__':
 	opt, args = parse_args(sys.argv[1:])	
 	
 	logger.info("Selected Map File: {}".format(opt.mapfile))
-
 	map_data = read_map(opt.mapfile)
 	print(map_data.n_rows)
 	print(map_data.n_cols)
 	
 	map_data.map_pretty_print()
-	get_points(map_data)
+	selected_points = get_points(map_data, 3, 3)
+
+	print("Selected points: {}".format(selected_points))
 	
+
 
